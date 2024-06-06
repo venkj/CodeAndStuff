d30ab74d3ca94ea86c5bc38231f4cb60b5c4ee94
venkj <jagannathan.venkatesh@gmail.com>
2024-06-06 11:54:39 +0100

    LC 1249: Remove Invalid parentheses

--- String/1249_remove_invalid_parentheses.py
+++ String/1249_remove_invalid_parentheses.py
@@ -1,27 +1,16 @@
 def remove_invalid_parentheses(s: str) -> str:
-
-    # First pass to remove invalid closing parentheses
-    res = []
-    cnt = 0
-    for c in s:
-        if c == "(":
-            res.append(c)
-            cnt += 1
-        elif c == ")" and cnt > 0:
-            res.append(c)
-            cnt -= 1
-        elif c != ")":
-            res.append(c)
-
-    # Second pass to remove invalid opening parentheses
-    filtered = []
-    for c in res[::-1]:
-        if c == "(" and cnt > 0:
-            cnt -= 1
-        else:
-            filtered.append(c)
-
-    return "".join(filtered[::-1])
+    stack = []
+    s = list(s)
+    for i in range(len(s)):
+        if s[i] == "(":
+            stack.append(i)
+        elif s[i] == ")" and stack:
+            stack.pop()
+        elif s[i] == ")" and not stack:
+            s[i] = ""
+    for i in stack:
+        s[i] = ""
+    return "".join(s)
 
 
 # Example usage:
